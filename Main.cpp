2cd814d04ca0844ebcbf046c68410a0ff9cc8c1f
Omar Einea <eineao@gmail.com>
2017-01-08 17:24:24 +0400

    Add the Light Source

--- Main.cpp
+++ Main.cpp
@@ -5,26 +5,34 @@
 
 using namespace std;
 
-const int screenWidth = 1280;
-const int screenHeight = 720;
+// Window Width and Height
+const int w = 1280, h = 720;
 
 void myDisplay(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINES);
+	glVertex2i(w * 0.95, h);
+	glVertex2i(w * 0.95, 0);
+	glEnd();
+
 	glFlush();
 }
 
 void main(int argc, char** argv) {
 	glutInit(&argc, argv); 
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(screenWidth, screenHeight);
+	glutInitWindowSize(w, h);
 	glutInitWindowPosition(50, 50);
 	glutCreateWindow("Mirror Reflection Game");
 	glutDisplayFunc(myDisplay);
 	glClearColor(1, 1, 1, 0);
 	glColor3f(0, 0, 0);
 	glPointSize(8.0);
+	glLineWidth(4.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0, screenWidth, 0.0, screenHeight);
+	gluOrtho2D(0.0, w, 0.0, h);
 	glutMainLoop();
 }
