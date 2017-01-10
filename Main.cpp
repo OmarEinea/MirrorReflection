966730ffe34d2fb54f1c93ee4a922e46e7f2bdb7
Omar Einea <eineao@gmail.com>
2017-01-10 14:49:05 +0400

    Make Mirror Reflection Functional

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include <gl/Gl.h>
 #include <gl/glut.h>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 // Window Width and Height
@@ -18,6 +19,7 @@ public:
 class Vector {
 public:
 	double x, y;
+	Vector() : x(0), y(0) {}
 	Vector(double a, double b) : x(a), y(b) {}
 	Vector(Point p1, Point p2) : x(p1.x - p2.x), y(p1.y - p2.y) {}
 	Vector perp() {
@@ -26,12 +28,31 @@ public:
 	double dot(Vector v) {
 		return x * v.x + y * v.y;
 	}
+	double length() {
+		return sqrt(x * x + y * y);
+	}
+	Vector normal() {
+		double l = length();
+		return l != 0 ? Vector(x / l, y / l) : Vector();
+	}
+	Vector operator*(double num) {
+		return Vector(x * num, y * num);
+	}
+	Vector operator-(Vector v) {
+		return Vector(x - v.x, y - v.y);
+	}
+	Point operator+(Point p) {
+		return Point(p.x + x, p.y + y);
+	}
 };
 
 class Mirror {
 public:
 	Point p1, p2;
 	bool released;
+	double distance, t;
+	Vector normal;
+	int index;
 	Mirror(Point a) : p1(a), p2(a), released(0) {}
 	Mirror(Point a, Point b) : p1(a), p2(b), released(1) {}
 };
@@ -43,33 +64,57 @@ public:
 		points.push_back(Point(w * 0.95, -h));
 		points.push_back(Point(w * 0.95, 2 * h));
 	}
-	bool intersects(Mirror m) {
-		Vector d = Vector(m.p1, m.p2);
-		Vector dPerp = d.perp();
-		for (int i = 1; i < points.size(); i++) {
-			Vector b(points[i - 1], points[i]);
-			Vector c(m.p1, points[i - 1]), bPerp = b.perp();
-			if (b.dot(dPerp) != 0) {
-				double t = - c.dot(dPerp) / b.dot(dPerp);
-				double u = c.dot(bPerp) / d.dot(bPerp);
-				return 0 <= t && t <= 1 && 0 <= u && u <= 1;
+	int calcClosestReflection(vector<Mirror> mirrors) {
+		int size = points.size() - 1;
+		Vector b(points[size - 1], points[size]), bPerp = b.perp();
+		vector<Mirror> intersecting;
+		for (int i = 0; i < mirrors.size(); i++) {
+			Mirror m = mirrors[i];
+			Vector d = Vector(m.p1, m.p2);
+			Vector dPerp = d.perp();
+			Vector c(m.p1, points[size - 1]);
+			double t = -c.dot(dPerp) / b.dot(dPerp);
+			double u = c.dot(bPerp) / d.dot(bPerp);
+			if (b.dot(dPerp) != 0 && 0 <= t && t <= 1 && 0 <= u && u <= 1) {
+				m.distance = Vector(points[size - 1], b * -t + points[size - 1]).length();
+				m.t = -t;
+				m.normal = d.normal();
+				m.index = i;
+				intersecting.push_back(m);
 			}
 		}
-		return false;
+		if (intersecting.size() == 0)
+			return -1;
+		Mirror closest = intersecting.front();
+		for (Mirror m : intersecting)
+			if (m.distance < closest.distance)
+				closest = m;
+		points[size] = b * closest.t + points[size - 1];
+		points.push_back(b - closest.normal * 2 * b.dot(closest.normal) + points[size]);
+		return closest.index;
 	}
 };
 
 vector<Mirror> mirrors;
-Light light;
 
 void myDisplay(void) {
+	Light light;
+	vector<Mirror> mirrorsLeft = mirrors;
+	for (int i = mirrorsLeft.size(); i > 0; i--) {
+		int index = light.calcClosestReflection(mirrorsLeft);
+		if(index > 0)
+			mirrorsLeft.erase(mirrorsLeft.begin() + index);
+	}
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_LINES);
+	glBegin(GL_LINE_STRIP);
 	// Draw Light Source
 	glColor3f(1, 0, 0);
 	for (Point p : light.points)
 		glVertex2i(p.x, p.y);
+	glEnd();
+
+	glBegin(GL_LINES);
 	// Draw Mirrors
 	glColor3f(0, 0, 1);
 	for (Mirror m : mirrors) {
